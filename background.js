eda2a8660a5f8643be7b520f3c8e05da48c9ce29
shubhampatil212 <cybersleuth23@gmail.com>
2025-01-04 23:42:44 +0530

    Initial commit: Added Chrome extension for YouTube search

---
+++ background.js
@@ -0,0 +1,18 @@
+// Create a context menu item
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "searchYouTube",
+      title: "Search YouTube for '%s'",
+      contexts: ["selection"] // Show only when text is selected
+    });
+  });
+  
+  // Add functionality when the context menu item is clicked
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "searchYouTube") {
+      const selectedText = info.selectionText;
+      const youtubeSearchURL = `https://www.youtube.com/results?search_query=${encodeURIComponent(selectedText)}`;
+      chrome.tabs.create({ url: youtubeSearchURL }); // Open a new tab with the YouTube search
+    }
+  });
+  
